fb029cb1c58facab47e6b5cd65c76fed6de83b87
sanyam-harness <sanyam.jain@harness.io>
2025-07-04 09:48:17 +0530

    Update handler.go

--- handler.go
+++ handler.go
@@ -4,80 +4,62 @@ import (
 	"encoding/json"
 	"net/http"
 	"strconv"
-	"time"
 
 	"github.com/gorilla/mux"
 )
 
-// In-memory store for todos and ID generator
-var todos = make(map[int]*Todo)
-var idCounter = 1
-
-// List all non-deleted todos
-func listTodos(w http.ResponseWriter, r *http.Request) {
-	var activeTodos []*Todo
-	for _, todo := range todos {
-		if !todo.Deleted {
-			activeTodos = append(activeTodos, todo)
-		}
-	}
-	json.NewEncoder(w).Encode(activeTodos)
+type Handler struct {
+	service *TodoService
 }
 
-// Create a new todo
-func createTodo(w http.ResponseWriter, r *http.Request) {
+func NewHandler(service *TodoService) *Handler {
+	return &Handler{service: service}
+}
+
+func (h *Handler) ListTodos(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(h.service.ListTodos())
+}
+
+func (h *Handler) CreateTodo(w http.ResponseWriter, r *http.Request) {
 	var todo Todo
 	if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	todo.ID = idCounter
-	todo.CreatedAt = time.Now()
-	todo.UpdatedAt = time.Now()
-	todos[todo.ID] = &todo
-	idCounter++
-	json.NewEncoder(w).Encode(todo)
+	created := h.service.CreateTodo(&todo)
+	json.NewEncoder(w).Encode(created)
 }
 
-// Get a single todo by ID
-func getTodo(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) GetTodo(w http.ResponseWriter, r *http.Request) {
 	id, _ := strconv.Atoi(mux.Vars(r)["id"])
-	todo, ok := todos[id]
-	if !ok || todo.Deleted {
+	todo, err := h.service.GetTodo(id)
+	if err != nil {
 		http.Error(w, "Todo not found", http.StatusNotFound)
 		return
 	}
 	json.NewEncoder(w).Encode(todo)
 }
 
-// Update an existing todo
-func updateTodo(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) UpdateTodo(w http.ResponseWriter, r *http.Request) {
 	id, _ := strconv.Atoi(mux.Vars(r)["id"])
-	todo, ok := todos[id]
-	if !ok || todo.Deleted {
-		http.Error(w, "Todo not found", http.StatusNotFound)
-		return
-	}
-	var updatedTodo Todo
-	if err := json.NewDecoder(r.Body).Decode(&updatedTodo); err != nil {
+	var updated Todo
+	if err := json.NewDecoder(r.Body).Decode(&updated); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	todo.Title = updatedTodo.Title
-	todo.Completed = updatedTodo.Completed
-	todo.UpdatedAt = time.Now()
-	json.NewEncoder(w).Encode(todo)
-}
-
-// Soft delete a todo
-func deleteTodo(w http.ResponseWriter, r *http.Request) {
-	id, _ := strconv.Atoi(mux.Vars(r)["id"])
-	todo, ok := todos[id]
-	if !ok || todo.Deleted {
+	todo, err := h.service.UpdateTodo(id, &updated)
+	if err != nil {
 		http.Error(w, "Todo not found", http.StatusNotFound)
 		return
 	}
-	todo.Deleted = true
-	todo.UpdatedAt = time.Now()
+	json.NewEncoder(w).Encode(todo)
+}
+
+func (h *Handler) DeleteTodo(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	if err := h.service.DeleteTodo(id); err != nil {
+		http.Error(w, "Todo not found", http.StatusNotFound)
+		return
+	}
 	w.WriteHeader(http.StatusNoContent)
 }
